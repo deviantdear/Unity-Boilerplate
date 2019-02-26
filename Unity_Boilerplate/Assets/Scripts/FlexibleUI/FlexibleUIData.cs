437dfc71be2683b64554c19804e4d1cc11cf3032
Valarie Adams <deviantdear@gmail.com>
2019-02-26 15:01:14 -0700

    Added TMPro, Menu assets and scripts
    Took existing menu scripts from the Unity Assets stores free Game Jam Menu Template  and editing them to run with TMPro
    Added working folder structure including Streaming Assets

---
+++ Unity_Boilerplate/Assets/Scripts/FlexibleUI/FlexibleUIData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+[CreateAssetMenu(menuName = "Flexible UI Data")]
+public class FlexibleUIData : ScriptableObject
+{
+
+    [Header("Button Colors")]
+    public ColorBlock buttonColorBlock;
+
+    [Header("Panel Color")]
+    public Color imageColor;
+
+    [Header("Slider Colors")]
+    public ColorBlock sliderColorBlock;
+
+    [Header("Text Attributes")]
+    public TMP_FontAsset font;
+    public Color32 textColor;
+}
---
+++ Unity_Boilerplate/Assets/Scripts/FlexibleUI/FlexibleUIText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FlexibleUIText : FlexibleUI
+
+{
+    private TextMeshProUGUI text;
+    public bool useMainFont = true;
+
+    void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        base.Initialize();
+    }
+
+    protected override void OnSkinUI()
+    {
+        base.OnSkinUI();
+        text.color =  flexibleUIData.textColor;
+        if (useMainFont)
+        {
+            text.font = flexibleUIData.font;
+        }
+        
+    }
+
+
+
+}
